9139743504c4eff58a30bcca10556a31853b7e7e
Jesse Osiecki <osiecki@live.unc.edu>
2013-09-09 13:33:13 -0400

    sed up some stuff trying to organize code before it gets big

--- bookBox.py
+++ bookBox.py
@@ -2,41 +2,52 @@
 
 from buzhug import Base
 import json
+import os
+#import book
 
-userdir = '/home/jesse/projects/TarHeelRanker/TarHeelRanker'
-db = Base(userdir + '/db/bookDB').open()
-resultset = db.select(['json'])
 
-for r in resultset:
+class bookDBHandler:
 
-    #load specific book's json from bookDB
-    jsonObj = json.loads(r.json)
+    def __init__(self):
 
-    #put all relavant info into convienient fields. I'm sure there is a quicker way involving commas with Python, will look at later
-    status = jsonObj.get('status')
-    rating_count = jsonObj.get('rating_count')
-    slug = jsonObj.get('slug')
-    language = jsonObj.get('language')
-    author = jsonObj.get('author')
-    rating_value = jsonObj.get('rating_value')
-    title = jsonObj.get('title')
-    modified = jsonObj.get('modified')
-    bust = jsonObj.get('bust')
-    reviewed = jsonObj.get('reviewed')
-    audience = jsonObj.get('audience')
-    tags = jsonObj.get('tags')
-    created = jsonObj.get('created')
-    link = jsonObj.get('link')
-    author_id = jsonObj.get('author_id')
-    iD = jsonObj.get('ID')
-    typex = jsonObj.get('type')
-    pages = jsonObj.get('pages')
-    categories = jsonObj.get('categories')
-    rating_total = jsonObj.get('rating_total')
+        self.userdir = os.getcwd()
+        self.bookDB = Base(self.userdir + '/db/bookDB').open()
+        #this selects the whole db, even though it specifies json as a requirement
+        self.resultset = self.bookDB.select()
 
-    #printing pages for fun. Have to use pages json tag, then treat that obj as dict; 'text' has the text
-    for p in pages:
-        print p['text']
-    print '------'
-db.cleanup()
-db.close()
+    def loadDBIn(self):
+        for r in self.resultset:
+            #load specific book's json from bookDB
+            jsonObj = json.loads(r.json)
+            dbID = r.__id__
+            #put all relavant info into convienient fields. I'm sure there is a quicker way involving commas with Python, will look at later
+            status = jsonObj.get('status')
+            rating_count = jsonObj.get('rating_count')
+            slug = jsonObj.get('slug')
+            language = jsonObj.get('language')
+            author = jsonObj.get('author')
+            rating_value = jsonObj.get('rating_value')
+            title = jsonObj.get('title')
+            modified = jsonObj.get('modified')
+            bust = jsonObj.get('bust')
+            reviewed = jsonObj.get('reviewed')
+            audience = jsonObj.get('audience')
+            tags = jsonObj.get('tags')
+            created = jsonObj.get('created')
+            link = jsonObj.get('link')
+            author_id = jsonObj.get('author_id')
+            iD = jsonObj.get('ID')
+            typex = jsonObj.get('type')
+            pages = jsonObj.get('pages')
+            categories = jsonObj.get('categories')
+            rating_total = jsonObj.get('rating_total')
+            text = []
+            #do the same for the pages
+            for p in pages:
+                text.append(p['text']
+            #insert this all into a nice little object
+            #thisBook =
+
+    def closeDB(self):
+        self.bookDB.cleanup()
+        self.bookDB.close()

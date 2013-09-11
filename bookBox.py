afb08386d644839f09bfe7bccde0f0991dc0c4b4
Jesse Osiecki <osiecki@live.unc.edu>
2013-09-11 11:20:34 -0400

    Made a nice cript for my convienience

--- bookBox.py
+++ bookBox.py
@@ -1,9 +1,45 @@
-#Jesse Osiecki for GB's Tar Heel Reader
+#Jesse Osiecki for GBs Tar Heel Reader
 
 from buzhug import Base
 import json
 import os
-#import book
+
+
+class authorCheck:
+    def __init__(self, books):
+        self.books = books
+
+    def check(self):
+        for book in self.books:
+            if book.reviewed:
+                print 'true'
+
+
+class book:
+    def __init__(self, dd, s, rc, au, rv, tt, md,
+                 rd, ad, tg, cd, lk,
+                 ai, i, tx, pg, ct,
+                 rt, txt):
+        self.dbID = dd
+        self.status = s
+        self.rating_count = rc
+        self.author = au
+        self.rating_value = rv
+        self.title = tt
+        self.modified = md
+        self.reviewed = rd
+        self.audience = ad
+        self.tags = tg
+        self.created = cd
+        self.link = lk
+        self.author_ID = ai
+        self.iD = i
+        self.typex = tx
+        self.pages = pg
+        self.categories = ct
+        self.rating_total = rt
+        self.text = txt
+        print self.dbID, ' ', self.iD, ' ', self.title
 
 
 class bookDBHandler:
@@ -12,24 +48,29 @@ class bookDBHandler:
 
         self.userdir = os.getcwd()
         self.bookDB = Base(self.userdir + '/db/bookDB').open()
-        #this selects the whole db, even though it specifies json as a requirement
+        #this selects the whole db, for now
         self.resultset = self.bookDB.select()
+
+        #for loading the db in memory
+        self.membooks = []
 
     def loadDBIn(self):
         for r in self.resultset:
             #load specific book's json from bookDB
             jsonObj = json.loads(r.json)
             dbID = r.__id__
-            #put all relavant info into convienient fields. I'm sure there is a quicker way involving commas with Python, will look at later
+            #put all relavant info into convienient fields.
+            #Im sure there is a quicker way involving commas
+            #with Python, will look at later
             status = jsonObj.get('status')
             rating_count = jsonObj.get('rating_count')
-            slug = jsonObj.get('slug')
-            language = jsonObj.get('language')
+            #slug = jsonObj.get('slug')
+            #language = jsonObj.get('language')
             author = jsonObj.get('author')
             rating_value = jsonObj.get('rating_value')
             title = jsonObj.get('title')
             modified = jsonObj.get('modified')
-            bust = jsonObj.get('bust')
+            #bust = jsonObj.get('bust')
             reviewed = jsonObj.get('reviewed')
             audience = jsonObj.get('audience')
             tags = jsonObj.get('tags')
@@ -44,9 +85,17 @@ class bookDBHandler:
             text = []
             #do the same for the pages
             for p in pages:
-                text.append(p['text']
-            #insert this all into a nice little object
-            #thisBook =
+                text.append(p['text'])
+
+            #nice little book object
+            #print dbID
+            #break
+            elBook = book(dbID, status, rating_count,
+                          author, rating_value, title, modified,
+                          reviewed, audience, tags, created, link,
+                          author_id, iD, typex, pages, categories,
+                          rating_total, text)
+            self.membooks.append(elBook)
 
     def closeDB(self):
         self.bookDB.cleanup()
---
+++ runScripts.py
@@ -0,0 +1,7 @@
+import bookBox as b
+
+bhandle = b.bookDBHandler()
+bhandle.loadDBIn()
+bhandle.membooks[0].reviewed
+#authorc = b.authorCheck(bhandle.membooks)
+#authorc.check()
